c3610d7731b0c8b2627544deaa2b6825f5436c20
Zachary Smith <zackmsmith10@gmail.com>
2023-12-03 14:37:32 -0800

    added urls.py to /helloworld

--- hempdb/urls.py
+++ hempdb/urls.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+from django.urls import include, path
+
 """
 URL configuration for hempdb project.
 
@@ -18,5 +21,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path("test/", include("helloworld.urls")),
     path('admin/', admin.site.urls),
 ]
