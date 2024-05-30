9c1f0baf0dbe04c8edd5b97e1c9c00730dd4b35f
Paul Mac <plipp2804@gmail.com>
2024-05-29 20:33:36 -0700

    fixes

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+python3 --version
+
 echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Collecting static files..."
-python3.12 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
