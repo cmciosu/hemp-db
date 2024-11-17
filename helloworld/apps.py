74cc7d8519a664cda4178a3d0d0060e7c1f6e967
Colton Melhase <coltonmelhase12@gmail.com>
2024-11-17 02:18:44 -0800

    Add ready function to load signals on startup

--- helloworld/apps.py
+++ helloworld/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class HelloworldConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "helloworld"
+
+    # Runs on startup
+    def ready(self):
+        import helloworld.signals   # Loads the signals defined in signals.py
