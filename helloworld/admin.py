c20c652ac70670d9162f375d6369c6ffcbd82676
Colton Melhase <coltonmelhase12@gmail.com>
2024-11-17 03:57:49 -0800

    Configure Django Administration dashboard header/title

--- helloworld/admin.py
+++ helloworld/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+
+# Customize Django Administration header/title
+admin.site.site_header = "HempDB Administration"
+admin.site.site_title = "HempDB Admin Portal"
+# admin.site.index_title = "HempDB Admin"
