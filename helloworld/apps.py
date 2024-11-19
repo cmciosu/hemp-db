be512da8d90a5d911b46978df585ad0f41464823
Colton Melhase <coltonmelhase12@gmail.com>
2024-11-19 14:17:39 -0800

    Explicitly connect signal to satisfy ruff lint

--- helloworld/apps.py
+++ helloworld/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import m2m_changed
 
 class HelloworldConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -7,4 +7,5 @@ class HelloworldConfig(AppConfig):
 
     # Runs on startup
     def ready(self):
-        import helloworld.signals   # Loads the signals defined in signals.py
+        import helloworld.signals   # Connect the signal handlers defined in signals.py
+        m2m_changed.connect(helloworld.signals.update_is_staff_on_group_change) # Explicitly connect (Satisfies ruff check)
