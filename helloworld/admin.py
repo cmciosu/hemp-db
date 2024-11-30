975fe454dcd7b25a72c761dc8f0caf4781820186
Colton Melhase <coltonmelhase12@gmail.com>
2024-11-29 22:55:20 -0800

    Add Company, PendingCompany, PendingChanges to admin dashboard

--- helloworld/admin.py
+++ helloworld/admin.py
@@ -1,1 +1,17 @@
+from django.contrib import admin
+from .models import Company, PendingCompany, PendingChanges
+
 # Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ["Name", "id"]
+    search_fields = ["Name"]
+
+@admin.register(PendingCompany)
+class PendingCompanyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PendingChanges)
+class PendingChangesAdmin(admin.ModelAdmin):
+    pass
