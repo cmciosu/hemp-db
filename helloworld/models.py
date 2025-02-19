1a61f1eed4f130369b09161fdb27bbd20ffb34a5
canfieca <canfieca@oregonstate.edu>
2025-02-18 19:08:26 -0800

    Made priority nullable

--- helloworld/models.py
+++ helloworld/models.py
@@ -30,7 +30,7 @@ class Resources(models.Model):
     url = models.CharField(max_length=1024, blank=True)
     text = models.CharField(max_length=2048, blank=True)
     image = models.CharField(max_length=1024, blank=True)
-    priority = models.SmallIntegerField(blank=True)
+    priority = models.SmallIntegerField(blank=True, null=True)
 
     class Meta:
         db_table = "resource"
