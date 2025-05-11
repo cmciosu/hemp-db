32a630b41adb06d39b70d12fe26a2f5232566258
Colton Melhase <coltonmelhase12@gmail.com>
2025-05-11 12:06:18 -0700

    Show change status inside admin portal

--- helloworld/admin.py
+++ helloworld/admin.py
@@ -1,4 +1,6 @@
 from django.utils import timezone
+from django.utils.html import format_html
+from django.urls import reverse
 
 from django.contrib import admin
 
@@ -40,8 +42,47 @@ class PendingCompanyAdmin(admin.ModelAdmin):
 
 @admin.register(PendingChanges)
 class PendingChangesAdmin(admin.ModelAdmin):
-    list_display = ["changeType", "author", "created_at_pst"]
+    list_display = ["company_link", "changeType", "author", "colored_status", "created_at_pst"]
     
+    search_fields = ['author__username', 'author__first_name', 'author__last_name']
+
+    # Creates the hyperlink based off the change type.
+    # e.i. create types only have pending_company foreign keys
+    # edit and deletion types relate with a company foreign key
+    def company_link(self, obj):
+        try:
+            if obj.changeType == "create" and obj.pending_company:
+                url = reverse('admin:helloworld_pendingcompany_change', args=[obj.pending_company.id])
+                name = str(obj.pending_company.Name)
+
+            elif (obj.changeType == "edit" or obj.changeType == "deletion") and obj.company:
+                url = reverse('admin:helloworld_company_change', args=[obj.company.id])
+                name = str(obj.company.Name)
+
+            else:
+                return "-"
+            
+            return format_html('<a href="{}">{}</a>', url, name)
+        
+        except Exception as e:
+            return f"Error: {e}"
+        
+    company_link.short_description = "Company"
+
+    def colored_status(self, obj):
+        if obj.status == PendingChanges.PendingStatus.PENDING:
+            color = 'orange'
+        elif obj.status == PendingChanges.PendingStatus.APPROVED:
+            color = 'green'
+        elif obj.status == PendingChanges.PendingStatus.REJECTED:
+            color = 'red'
+        else:
+            color = 'gray'
+    
+        return format_html('<span style="color: {};">{}</span>', color, obj.get_status_display())
+    
+    colored_status.short_description = "Status"
+
     def created_at_pst(self, obj):
         local_dt = timezone.localtime(obj.created_at, timezone.get_fixed_timezone(-480)) # UTC-8 (PST)
         return local_dt.strftime('%Y-%m-%d %H:%M:%S')
